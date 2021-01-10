b877a1d56bc467bcf4579c6edfbf7c2f898ea77a
kureharyosuke <ohhayo.com@gmail.com>
2021-01-10 13:36:32 +0900

    v-on:submit = @submit

---
+++ src/components/GuGuDan.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <div>{{ first }} X {{ second }}?</div>
+    <form @submit="onSubmitForm">
+      <input type="number" ref="answer" v-model="value" />
+      <button type="subimt" @click="onClickButton">Button</button>
+      <!-- <div>{{ value }}</div> -->
+    </form>
+    <div id="result">{{ result }}</div>
+  </div>
+</template>
+<script>
+export default {
+  name: "GuGuDan",
+  // React = state , vue =  data?
+  data() {
+    return {
+      first: Math.ceil(Math.random() * 9),
+      second: Math.ceil(Math.random() * 9),
+      value: "",
+      result: ""
+    };
+  },
+  methods: {
+    onSubmitForm(e) {
+      e.preventDefault();
+      console.log(this);
+      console.log(this.first, this.second, this.value);
+      if (this.first * this.second === parseInt(this.value)) {
+        this.result = "Yes";
+        this.first = Math.ceil(Math.random() * 9);
+        this.second = Math.ceil(Math.random() * 9);
+        this.value = "";
+        // this.input.focus();
+        this.$refs.answer.focus();
+      } else {
+        this.result = "No";
+        this.value = "";
+        // this.input.focus();
+        this.$refs.answer.focus();
+      }
+    },
+    onClickButton() {}
+  }
+};
+</script>
+
+<style></style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Liked from "@/components/Liked";
+import GuGuDan from "@/components/GuGuDan";
 
 Vue.use(Router);
 
@@ -10,6 +11,11 @@ export default new Router({
       path: "/",
       name: "Liked",
       component: Liked
+    },
+    {
+      path: "/gugudan",
+      name: "GuGuDan",
+      component: GuGuDan
     }
   ]
 });
