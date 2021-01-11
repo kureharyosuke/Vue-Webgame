a374a85244ddf2289513489ddfcae49d08f34c3b
kureharyosuke <ohhayo.com@gmail.com>
2021-01-11 14:44:58 +0900

    #5. v-for Error: v-bind:key ?

--- src/components/NumberBaseball.vue
+++ src/components/NumberBaseball.vue
@@ -16,10 +16,22 @@
 </template>
 
 <script>
+const getNumbers = () => {
+  const candidates = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const array = [];
+  for (let i = 0; i < 4; i += 1) {
+    const number = candidates.splice(Math.floor(Math.random() * (9 - i)), 1)[0];
+    array.push(number);
+  }
+  //   console.log(number);
+  return array;
+};
+
 export default {
   name: "NumberBaseBall",
   data() {
     return {
+      answer: getNumbers(), // ex) [1,2,6,7] = this.answer.join('')
       tries: [],
       value: "",
       result: ""
@@ -27,10 +39,40 @@ export default {
   },
   methods: {
     onSubmitForm() {
-      this.tries.push({
-        try: this.value,
-        result: "HOME RUN!"
-      });
+      if (this.value === this.answer.join("")) {
+        // 정답이면
+        this.tries.push({
+          try: this.value,
+          result: "HOME RUN!"
+        });
+        this.result = "HOME RUN!";
+        this.tries = [];
+        this.$refs.answer.focus();
+      } else {
+        // 틀리면
+        if (this.tries.length >= 9) {
+          // 10번째 시도
+          this.result = ` Game Over... ${this.answer.join(",")} 였습니다.`;
+          alert("New Game Start!");
+          (this.value = ""), (this.answer = getNumbers());
+          (this.tries = []), this.$refs.answer.focus();
+        }
+        let strike = 0;
+        let ball = 0;
+        const answerArray = this.value.split("").map(v => parseInt(v));
+        for (let i = 0; i < 4; i += 1) {
+          if (answerArray[i] === this.answer[i]) {
+            // 숫자 자릿수 모두 정답
+            strike++;
+          } else if (this.answer.includes(answerArray[i])) {
+            ball++;
+          }
+        }
+        this.tries.push({
+          try: this.value,
+          result: `${strike} Strike, ${ball} Ball.`
+        });
+      }
       this.value = "";
       this.$refs.answer.focus();
     }
